922b12e72716eb59abed62ec6e5b476d3314cc6e
micheal head <michealh.5678@yahoo.com>
2015-04-23 17:26:01 -0500

    changed Hero to reflect change to Character
    - added name stuff yes

--- Hero.java
+++ Hero.java
@@ -2,7 +2,7 @@ public class Hero extends Character {
 	
 	int potion = 0;
 	
-	public Hero(int h, int a, int aE, int hE) {
-		super(h, a, aE, hE);
+	public Hero(String n, int h, int a, int aE, int hE) {
+		super(n, h, a, aE, hE);
 	}
 }
