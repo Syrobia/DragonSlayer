5cb2c228180732bb90050e55597006d591ab3816
SwagmanJet <ajet456@gmail.com>
2015-04-24 12:23:21 -0500

    Update Inventory.java
    Fixed Armor class

--- Inventory.java
+++ Inventory.java
@@ -1,27 +1,28 @@
 //package com.github.syrobia.dragonslayer.inventory;
 
 /*TODO
- *Create Armor Class
+ *Create Armor Class - Done
+ *Create Potion Class
  */
 public class Inventory
 {
   Item[][] all = new Item[3][4];
   
-  public void addArmor(String armor)
+  public void addArmor(Armor armor)
   {
-    if (armor.indexOf("helm") != -1)
+    if (armor.getClass() == "Helmet")
     {
       all[0][0] = armor;
     }
-    else if (armor.indexOf("chest") != -1)
+    else if (armor.getClass() == "Chestplate")
     {
       all[0][1] = armor;
     }
-    else if (armor.indexOf("leg") != -1)
+    else if (armor.getClass() == "Leggings")
     {
       all[0][2] = armor;
     }
-    else if (armor.indexOf("boot") != -1)
+    else if (armor.getClass() == "Boots")
     {
       all[0][3] = armor;
     }
