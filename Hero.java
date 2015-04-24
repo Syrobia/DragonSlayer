9114cb09a1dd42a9c807222562f2efb4133e164f
Ammad Hashmi <ammadxx1@gmail.com>
2015-04-24 11:54:55 -0500

    CC
    Compiled and Check-No Syntax error found

--- Hero.java
+++ Hero.java
@@ -1,8 +1,8 @@
 public class Hero extends Character {
-	
-	int potion = 0;
-	
-	public Hero(String n, int h, int a, int aE, int hE) {
-		super(n, h, a, aE, hE);
-	}
+ 
+ int potion = 0;
+ 
+ public Hero(String n, int h, int a, int aE, int hE) {
+  super(n, h, a, aE, hE);
+ }
 }
