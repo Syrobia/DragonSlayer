41282b9abe87db43b37eea44612449f686506735
SwagmanJet <ajet456@gmail.com>
2015-04-24 12:19:13 -0500

    Update Armor.java
    #420BLAZEIT

--- Armor.java
+++ Armor.java
@@ -1,7 +1,7 @@
 public class Armor extends Item
 {
   private int protection;
-  private String class;
+  private String clase;
   
   public Armor (String name)
   {
@@ -12,26 +12,37 @@ public class Armor extends Item
   public void effect()
   {
     //lolcats
-    String name = super.returnName();
+    String name = super.returnName().toLowerCase();
     if (name.indexOf("helm") >= 0)
     {
       protection = 10;
-      class = "Helmet";
+      clase = "Helmet";
     }
     else if (name.indexOf("chest") >= 0)
     { 
       protection = 25;
-      class = "Chestplate";
+      clase = "Chestplate";
     }
     else if (name.indexOf("leg") >= 0)
     {
       protection = 15;
-      class = "Leggings";
+      clase = "Leggings";
     }
-    else (name.indexOf("boot") >= 0)
+    else if (name.indexOf("boot") >= 0)
     {
       protection = 5;
-      class = "Boots";
+      clase = "Boots";
+    }
+    
+    else
+    {
+      protection = 0;
+    }
+    
+    if (name.indexOf("syrobia")
+    {
+      protection = 420;
+      clase = "#BLAZEIT";
     }
   }
   
@@ -42,12 +53,12 @@ public class Armor extends Item
   
   public String getClass()
   {
-    return class;
+    return clase;
   }
   
   public String toString()
   {
-    return super.returnName() + "\nProtection Level: " + protection + "\nClass: " + class;
+    return super.returnName() + "\nProtection Level: " + protection + "\nClass: " + clase;
   }
 }
       
