cdb2dac1480648154ef34adf9f1e2cc20ba2d6bb
Ammad Hashmi <ammadxx1@gmail.com>
2015-04-24 17:48:00 -0500

    Create Potions.java
    *Basically copied the armor class and substituted with potion items
    *Anything else that needs to be made pertaining to potions, do so.
    *Possible Weapons Potion class that extends weapons instead??
    **Mage Character class?
    **Oh, different hero classes!!

---
+++ Potions.java
@@ -0,0 +1,63 @@
+public class Potion extends Item
+{
+  private int healthEffect;
+  private String clase;
+  
+  public Potion (String name)
+  {
+    super(name);
+    effect();
+  }
+  
+  public void effect()
+  {
+    //lolcats
+    String name = super.returnName().toLowerCase();
+    if (name.indexOf("normal") >= 0)
+    {
+      healthEffect = 10;
+      clase = "Normal";
+    }
+    else if (name.indexOf("strong") >= 0)
+    { 
+      healthEffect = 25;
+      clase = "Strong";
+    }
+    else if (name.indexOf("fire") >= 0)
+    {
+      healthEffect = -15;
+      clase = "Weak";
+    }
+    else if (name.indexOf("posion") >= 0)
+    {
+      healthEffect = -10;
+      clase = "Posion";
+    }
+    
+    else
+    {
+      healthEffect = 0;
+    }
+    
+    if (name.indexOf("syrobia")
+    {
+      healthEffect = 420;
+      clase = "#BLAZEIT";
+    }
+  }
+  
+  public int getHealthEffect()
+  {
+    return healthEffect;
+  }
+  
+  public String getClass()
+  {
+    return clase;
+  }
+  
+  public String toString()
+  {
+    return super.returnName() + "\Health Level: " + healthEffect + "\nClass: " + clase;
+  }
+}
