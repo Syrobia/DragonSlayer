13794f77752cf073fc40c8c5eada115d223c0982
Ammad Hashmi <ammadxx1@gmail.com>
2015-04-25 21:59:37 -0500

    Create Squire.java

---
+++ Squire.java
@@ -0,0 +1,11 @@
+public class Squire extends Character {
+/*TODO
+*Fix this (I just made it just to remind me to havea Squire class
+*/
+ 
+ int potion = 0;
+ 
+ public Squire(String n, int h, int a, int aE, int hE) {
+  super(n, h, a, aE, hE);
+ }
+}
