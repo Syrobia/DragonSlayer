feaf1f6c3e4034dba56beaa162110484266d8d75
Ammad Hashmi <ammadxx1@gmail.com>
2015-04-27 18:32:03 -0500

    Update DrawMain.java
    *Added DrawMain void constructor (can you have a void constructor?) anyways yea, to draw stuff

--- DrawMain.java
+++ DrawMain.java
@@ -1,9 +1,21 @@
 //Syrobia
 //package com.github.syrobia.dragonslayer.drawmain;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class DrawMain {
+	
+	public void DrawMain (Graphics g) {
+		Graphics2D e = (Graphics2D) g;
+	}
+	
+	//Runner	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("DragonSlayer -by Syrobia");
 		frame.setSize(420, 640);
