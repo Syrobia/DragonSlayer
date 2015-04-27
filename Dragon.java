83e1b9586bdb94aa9e9b7722f29b30491c8f44fc
micheal head <michealh.5678@yahoo.com>
2015-04-27 16:25:12 -0500

    CC
    ln80 - didn't have a return type that worked, fixed to an int[] length 2, values addedEffect and negativeEffect

--- Dragon.java
+++ Dragon.java
@@ -9,82 +9,84 @@ import java.util.Random;
 
 public class Dragon extends Character 
 {
-  private final int MAX_HEALTH = 750;
+   private final int MAX_HEALTH = 750;
   
-  private String typeOfDragon;
+   private String typeOfDragon;
  
- public Dragon(String n, int h, int a, int aE, int hE){
-  super(n, h, a, aE, hE);
- }
+   public Dragon(String n, int h, int a, int aE, int hE){
+      super(n, h, a, aE, hE);
+   }
  
- public int generateHealth(){
- Random generator = new Random();
- super.setHealth(generator.nextInt(3)+1);
- super.setHealth(super.getHealth() * 200);
- return super.getHealth();
- }
+   public int generateHealth(){
+      Random generator = new Random();
+      super.setHealth(generator.nextInt(3)+1);
+      super.setHealth(super.getHealth() * 200);
+      return super.getHealth();
+   }
  
- public int generateAttack(){
- Random generator = new Random();
- super.setAttack(generator.nextInt(3)+1);
- super.setAttack(super.getAttack() * 50);
- return super.getAttack();
- }
+   public int generateAttack(){
+      Random generator = new Random();
+      super.setAttack(generator.nextInt(3)+1);
+      super.setAttack(super.getAttack() * 50);
+      return super.getAttack();
+   }
  
- public String typeOfDragon(){
- int temp;
- Random generator = new Random();
- temp = generator.nextInt(6)+1; 
- if (temp == 1)
-  typeOfDragon = "fire";
- else if (temp == 2)
-  typeOfDragon = "ice";
- else if (temp == 3)
-  typeOfDragon = "rock";
- else if (temp == 4)
-  typeOfDragon = "lightning";
- else if (temp == 5)
-  typeOfDragon = "teja";
- else
-  typeOfDragon = "normal";
+   public String typeOfDragon(){
+      int temp;
+      Random generator = new Random();
+      temp = generator.nextInt(6)+1; 
+      if (temp == 1)
+         typeOfDragon = "fire";
+      else if (temp == 2)
+         typeOfDragon = "ice";
+      else if (temp == 3)
+         typeOfDragon = "rock";
+      else if (temp == 4)
+         typeOfDragon = "lightning";
+      else if (temp == 5)
+         typeOfDragon = "teja";
+      else
+         typeOfDragon = "normal";
+   
+      return typeOfDragon;
+   }
  
- return typeOfDragon;
- }
+   public int[] getEffect(){
+      int addedEffect = 0;
+      int negativeEffect = 0;
+      if (typeOfDragon.equals("fire"))
+         addedEffect = super.getAttack() + 75;
+      negativeEffect = super.getHealth() - 15;
+   
+      if (typeOfDragon.equals("lightning"))
+         addedEffect = super.getAttack() + 225;
+      negativeEffect = super.getHealth() - 25;
+   
+      if (typeOfDragon.equals("ice"))
+         addedEffect = super.getHealth() + 75;
+      negativeEffect = super.getAttack() - 15;
+   
+      if (typeOfDragon.equals("rock"))
+         addedEffect = super.getHealth() + 225;
+      negativeEffect = super.getAttack() - 220;
+   
+      if (typeOfDragon.equals("teja"))
+         addedEffect = super.getAttack() + 360;
+      negativeEffect = super.getHealth() + 100;
+   
+      if (addedEffect > MAX_HEALTH)
+         addedEffect = MAX_HEALTH;
+   
+      int[] returned = {addedEffect, negativeEffect};
+      return returned;
+   }
  
- public int[] getEffect(){
- int addedEffect = 0;
- int negativeEffect = 0;
- if (typeOfDragon.equals("fire"))
-  addedEffect = super.getAttack() + 75;
-  negativeEffect = super.getHealth() - 15;
-  
- if (typeOfDragon.equals("lightning"))
-  addedEffect = super.getAttack() + 225;
-  negativeEffect = super.getHealth() - 25;
-  
- if (typeOfDragon.equals("ice"))
-  addedEffect = super.getHealth() + 75;
-  negativeEffect = super.getAttack() - 15;
-  
- if (typeOfDragon.equals("rock"))
-  addedEffect = super.getHealth() + 225;
-  negativeEffect = super.getAttack() - 220;
-  
- if (typeOfDragon.equals("teja"))
-  addedEffect = super.getAttack() + 360;
-  negativeEffect = super.getHealth() + 100;
-  
- if (addedEffect > MAX_HEALTH)
-  addedEffect = MAX_HEALTH;
-
- return returned;
- }
+   public String toString()
+   {
+      return "";
+   }
  
- public String toString()
- {
- }
  
- }
 }
  
  
