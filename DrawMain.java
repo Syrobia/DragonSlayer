5be26033cda24ea3ff179ced55771fde67588130
Ammad Hashmi <ammadxx1@gmail.com>
2015-04-27 18:26:36 -0500

    Create DrawMain.java
    *Just starting our JPanel component of the game

---
+++ DrawMain.java
@@ -0,0 +1,13 @@
+//Syrobia
+//package com.github.syrobia.dragonslayer.drawmain;
+
+import javax.swing.JFrame;
+
+public class DrawMain {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("DragonSlayer -by Syrobia");
+		frame.setSize(420, 640);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
