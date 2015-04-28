dfe4ec3d6927858ee49c7e6eb7e2ed36f0fa1d84
SwagmanJet <ajet456@gmail.com>
2015-04-28 12:25:14 -0500

    Create Attributes.java
    Attributes class created:
    Constructor which takes index
    Static method which can find index

---
+++ Attributes.java
@@ -0,0 +1,36 @@
+//Syrobia
+//package com.github.syrobia.dragonslayer.attributes;
+
+public class Attributes{
+    
+  private String attribute = "";
+  private String[] attributeList = {"Fire", "Ice", "Water", "Lightning", "Swag", "Syrobia"};
+  
+  public Attribute(int index)
+  {
+    attribute = attributeList[index];
+  }
+  
+  public static int getIndex(String attr)
+  {
+    int index = 0;
+    boolean found = false;
+    
+    for (int i = 0; i < attributeList.length && found != true; i++)
+    {
+      if (attr == attributeList[i])
+      {
+        index = i;
+        found = true;
+      }
+    }
+    
+    if (found = false)
+    {
+      System.out.println ("That's not an attribute!");
+      return -1;
+    }
+    
+    return index;
+  }
+}
