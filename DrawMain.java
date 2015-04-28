e54bd57196b32aee39e9c8d52017a2ec530a0083
Ammad Hashmi <ammadxx1@gmail.com>
2015-04-27 19:32:27 -0500

    Creating Methods [Read Comments]
    *Just getting some information for JPanel
    *Some of these methods will not be used
    *This class may need to be completely restructured later on

--- DrawMain.java
+++ DrawMain.java
@@ -1,4 +1,4 @@
-//Syrobia
+//Syrobia-This is just a test class for the JPanel component --> This may also be used to start to implement the actual game?
 //package com.github.syrobia.dragonslayer.drawmain;
 
 import java.awt.*;
@@ -6,11 +6,25 @@ import java.awt.event.*;
 import javax.swing.*;
 
 
-public class DrawMain extends JPanel{
-	
+public class DrawMain extends JPanel implements MouseMotionListener{
+	private int mX, mY;
 	public DrawMain() {
 		addMouseListener (new DrawMain());
 		setBackground (Color.green);
+	}
+	
+	public void mouseMoved(MouseEvent me) {
+    		mX = (int) me.getPoint().getX();
+		mY = (int) me.getPoint().getY();
+	}
+	public void mouseDragged(MouseEvent me) {
+    		mouseMoved(me);
+	}
+	public void mousePressed(MouseEvent e) {
+		//put stuff here
+	}
+	public void mouseReleased(MouseEvent e){
+		//put stuff here 
 	}
 	
 	public void paintMain (Graphics g) {
