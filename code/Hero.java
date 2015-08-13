d6724c5d4dad8be2bfe12846765e25a774bfb3cc
micheal head <michealh.5678@yahoo.com>
2015-08-12 21:18:34 -0500

    Update and rename Hero.java to Player.java

--- code/Hero.java
+++
@@ -1,8 +0,0 @@
-public class Hero extends Character {
- 
- int potion = 0;
- 
- public Hero(String name, int h, int a, int aE, int hE) {
-  super(name, h, a, aE, hE);
- }
-}
---
+++ code/Player.java
@@ -0,0 +1,8 @@
+public class Player extends Character {
+ 
+  int potion = 0;
+ 
+  public Hero(String name, int h, int a, int aE, int hE) {
+    super(name, h, a, aE, hE);
+  }
+}
