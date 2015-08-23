58c156720bef85988ff66d6421c536b4dffa535c
micheal <michealh.5678@yahoo.com>
2015-08-23 17:13:49 -0500

    Update Player.java

--- code/Player.java
+++ code/Player.java
@@ -1,8 +1,29 @@
 public class Player extends Character {
  
-  int potion = 0;
+  Inventory inv = new Inventory();
  
-  public Hero(String name, int h, int a, int aE, int hE) {
-    super(name, h, a, aE, hE);
+  public Player()
+  {
+  	super();
+  }
+  
+  public Player(String n, int h, int a, int aE, int hE, int maxH)
+  {
+  	super(n,h,a,aE,hE,maxH);
+  }
+  
+  public void addArmor(Armor _armor)
+  {
+  	inv.addArmor(_armor);
+  }
+  
+  public void addPotion(Potion _pot)
+  {
+  	inv.addPotion(_pot);
+  }
+  
+  public void addWeapon(Weapon _weapon)
+  {
+  	inv.addWeapon(_weapon);
   }
 }
