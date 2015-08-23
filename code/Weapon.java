619a36b6227f3f7d7c7230c37055e261bef0975d
micheal <michealh.5678@yahoo.com>
2015-08-23 11:59:04 -0500

    Update Weapon.java
    - took out attributes
    - simplified the code

--- code/Weapon.java
+++ code/Weapon.java
@@ -1,7 +1,6 @@
 public class Weapon extends Item
 {
    private int damage;
-   private int attributeNumber;
    
    /*To Do:
      Add a way to add attributes.
@@ -15,32 +14,8 @@ public class Weapon extends Item
    
    public void effect()
    {
-      String name = getName().toLowerCase();
-      if (name.equals("syrobia"))
-      {
-         damage = 1337;
-         attributeNumber = 3;
-      }
-      else if (name.indexOf("sword") >= 0)
-      {
-         damage = 100;
-         attributeNumber = 1;
-      }
-      else if (name.indexOf("club") >= 0)
-      {
-         damage = 50;
-         attributeNumber = 2;
-      }
-      else if (name.indexOf("staff") >= 0)
-      {
-         damage = 25;
-         attributeNumber = 3;
-      }
-      else
-      {
          damage = (int) (Math.random() * 100) + 1;
          attributeNumber = (int) (Math.random() * 3) + 1;
-      }
    }
    
    public int getDamage()
@@ -48,13 +23,8 @@ public class Weapon extends Item
       return damage;
    }
    
-   public int getAttributeSlots()
-   {
-      return attributeNumber;
-   }
-   
    public String toString()
    {
-      return getName() + "\nDamage: " + damage + "\nAttribute Slots: " + attributeNumber;
+      return getName() + "\nDamage: " + damage;
    }
 }
